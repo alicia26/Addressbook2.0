a15b6df519ad2be64649a867bbda601721fbec72
eenat <etarnovskaya@hotmail.com>
2018-11-12 13:37:58 +0200

    created dataProvider for groups

--- repetition-03_10_2018/src/test/java/com/telran/repeat/tests/GroupCreationTests.java
+++ repetition-03_10_2018/src/test/java/com/telran/repeat/tests/GroupCreationTests.java
@@ -2,19 +2,38 @@ package com.telran.repeat.tests;
 
 import com.telran.repeat.model.Group;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class GroupCreationTests extends TestBase {
-@Test
-  public void testGroupCreation(){
+  @DataProvider
+  public Iterator<Object[]> validGroups() throws IOException {
+    List<Object[]>list = new ArrayList<>();
+    BufferedReader reader  = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+    String line = reader.readLine();
+    while (line != null){
+      String[] split = line.split(";");
+      list.add(new Object[] {new Group().withGroupName(split[0])
+              .withGroupHeader(split[1]).withGroupFooter(split[2])});
+      line = reader.readLine();
+    }
+    return list.iterator();
+  }
+@Test (dataProvider = "validGroups")
+  public void testGroupCreation(Group group){
   app.getNavigationHelper().openGroupsPage();
   int before = app.getGroupHelper().getGroupsCount();
 
   app.getGroupHelper().initGroupCreation();
-  app.getGroupHelper().fillGroupForm(new Group()
-                  .withGroupName("")
-                  .withGroupHeader("")
-                  .withGroupFooter(""));
+
+  app.getGroupHelper().fillGroupForm(group);
+
+
 //          .withGroupName("").withGroupHeader()
 //          .withGroupName("new"), "new 1", "new"));
   app.getGroupHelper().confirmGroupCreation();
